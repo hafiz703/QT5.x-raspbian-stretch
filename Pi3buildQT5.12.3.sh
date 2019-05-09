2e82c7667c469b1368553e44beb99b812e593814
Muhammad Hafiz <mhbt-703@hotmail.com>
2019-05-09 12:39:45 +0800

    Update Pi3buildQT5.12.3.sh

--- Pi3buildQT5.12.3.sh
+++ Pi3buildQT5.12.3.sh
@@ -54,7 +54,7 @@ PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/share/pkgconfig \
 -system-freetype \
 -fontconfig \
 -glib \
--prefix /opt/Qt5.12 \
+-prefix /opt/Qt5 \
 -qpa eglfs
 
 
